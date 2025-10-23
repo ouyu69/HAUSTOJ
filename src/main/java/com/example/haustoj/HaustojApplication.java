b46a0b19f5c446210786f56d4685251f8093c09a
ouyu69 <zhiyuanyang410@gmail.com>
2025-10-23 15:31:07 +0800

    init

---
+++ src/main/java/com/example/haustoj/HaustojApplication.java
@@ -0,0 +1,13 @@
+package com.example.haustoj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaustojApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaustojApplication.class, args);
+    }
+
+}
